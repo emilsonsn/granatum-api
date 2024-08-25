c87c221c978002791dba6ef95d0c67968a92179c
emilsonsn <emilsonsn2@gmail.com>
2024-08-25 10:33:03 -0300

    feat: adiciona seed de usuário

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -17,6 +17,12 @@ class UserSeed extends Seeder
             'name' => 'Admin',
             'email' => 'admin@admin',
             'password' => Hash::make('admin'),
+            'phone' => '83991236636',
+            'whatsapp' => '83991236636',
+            'cpf_cnpj' => '13754674412',
+            'birth_date' => '2001-12-18',
+            'company_position_id' => null,
+            'sector_id' => null,
             'is_active' => true,
             'is_admin' => true
         ]);
