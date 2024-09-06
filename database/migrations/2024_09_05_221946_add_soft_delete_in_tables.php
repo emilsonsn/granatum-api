92172ce80b6054c5763b9f939305d8c257c83d5b
emilsonsn <emilsonsn2@gmail.com>
2024-09-05 22:24:26 -0300

    feat: implementa softdelete no projeto

---
+++ database/migrations/2024_09_05_221946_add_soft_delete_in_tables.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('company_positions', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('constructions', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('orders', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('services', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('solicitations', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('releases', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tables', function (Blueprint $table) {
+            //
+        });
+    }
+};
