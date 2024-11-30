1ab8411e3fb96975513239202d4ff10ca0f3a9fa
emilsonsn <emilsonsn2@gmail.com>
2024-11-30 10:15:31 -0300

    fix: adiciona userseed em database seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(TaskStatusSeeder::class);
-        // $this->call(UserSeed::class);
+        $this->call(UserSeed::class);
         $this->call(CompanyPositionSeed::class);
     }
 }
