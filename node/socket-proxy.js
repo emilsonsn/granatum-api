34213ddb074d739dbb39b34293ed8cb57ab2143c
emilsonsn <emilsonsn2@gmail.com>
2024-12-09 22:12:28 -0300

    fix: adiciona mais intâncias ao socket

--- node/socket-proxy.js
+++ node/socket-proxy.js
@@ -1,26 +1,31 @@
 import { io } from 'socket.io-client';
 import axios from 'axios';
 
-// Conectar ao WebSocket da Evolution API usando socket.io
-const socket = io('https://api.andradeengenhariaeletrica.com.br/teste123'); 
+const instances = ['escritorio', 'teste123', 'mel'];
 
-// Quando conectado
-socket.on('connect', () => {});
+const baseUrl = "https://app.andradeengenhariaeletrica.com.br:3001/api/evolution-data";
+// const baseUrl = "http://localhost:8000/api/evolution-data";
 
-// Ouvir todos os eventos
-socket.onAny((eventName, data) => {
-    console.log(`Evento recebido: ${eventName}`, data);
-    
-    axios.post('http://localhost:8000/api/evolution-data', { event: eventName, data })
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(error => {
-            console.error('Erro ao enviar dados para o Laravel:', error.message);
-        });
-});
+instances.forEach((instance) => {
+    const socket = io(`https://api.andradeengenhariaeletrica.com.br/${instance}`); // Conecta com a instância
 
-// Desconectar
-socket.on('disconnect', () => {
-    console.log('Desconectado do WebSocket da Evolution API');
+    socket.on('connect', () => {
+        console.log(`Conectado ao WebSocket da instância: ${instance}`);
+    });
+
+    socket.onAny((eventName, data) => {
+        console.log(`Evento recebido (${instance}): ${eventName}`, data);
+
+        axios.post(baseUrl, { event: eventName, data, instance })
+            .then(response => {
+                console.log(`Resposta recebida (${instance}):`, response.data);
+            })
+            .catch(error => {
+                console.error(`Erro ao enviar dados (${instance}):`, error.message);
+            });
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Desconectado do WebSocket da instância: ${instance}`);
+    });
 });
